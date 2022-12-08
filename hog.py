637500fbf1f3eb8a7be4200a03e619d0655a6418
vasu14082000 <vasudev_agarwal@brown.edu>
2022-12-08 15:00:39 +0530

    HOG
    Push for calculating average descriptor similarity between all combinations of colors

--- hog.py
+++ hog.py
@@ -22,11 +22,10 @@ def get_hog_features(image):
     for i in range(0,120,8):
         for j in range(0,56,8):
             x += 1
-            print(x)
             block_hist = get_hist(i,j,magnitudes,orientations)
             feature_vector = np.concatenate((feature_vector,block_hist)) 
     
-    return np.reshape(feature_vector,(15,7,36))
+    return feature_vector
 
 
 def get_hist(i,j,magnitudes,orientations):
@@ -44,8 +43,62 @@ def get_hist(i,j,magnitudes,orientations):
     # normalized_hist = block_hist/np.sqrt(np.linalg.norm(block_hist) + 0.00001)
     return normalized_hist
 
-my_descriptor = get_hog_features("/Users/macuser/Desktop/2022-2023/Semester 3/CSCI1430/finalprojectCSCI1430/hog_car.jpg")
-print(np.shape(my_descriptor))
+nums = ["1","2","3","4","5","6","7"]
+org = []
+red = []
+prp = []
+o_r = []
+o_p = []
+r_p = []
+for i in range(0,7):
+    x = nums[i]
+    org1 = get_hog_features("/Users/vasudev/Desktop/CSCI1430/finalprojectCSCI1430/colorLocator/results/left ("+x+")/orange.jpeg")
+    red1 = get_hog_features("/Users/vasudev/Desktop/CSCI1430/finalprojectCSCI1430/colorLocator/results/left ("+x+")/red.jpeg")
+    prp1 = get_hog_features("/Users/vasudev/Desktop/CSCI1430/finalprojectCSCI1430/colorLocator/results/left ("+x+")/purple.jpeg")
+    for j in range(i,7):
+        y = nums[j]
+        org2 = get_hog_features("/Users/vasudev/Desktop/CSCI1430/finalprojectCSCI1430/colorLocator/results/left ("+y+")/orange.jpeg")
+        red2 = get_hog_features("/Users/vasudev/Desktop/CSCI1430/finalprojectCSCI1430/colorLocator/results/left ("+y+")/red.jpeg")
+        prp2 = get_hog_features("/Users/vasudev/Desktop/CSCI1430/finalprojectCSCI1430/colorLocator/results/left ("+y+")/purple.jpeg")
+
+        c_org = np.dot(org1,org2)/(np.linalg.norm(org1)*np.linalg.norm(org2))
+        c_red = np.dot(red1,red2)/(np.linalg.norm(red1)*np.linalg.norm(red2))
+        c_prp = np.dot(prp1,prp2)/(np.linalg.norm(prp1)*np.linalg.norm(prp2))
+        c_o_r = np.dot(org1,red2)/(np.linalg.norm(org1)*np.linalg.norm(red2))
+        c_o_p = np.dot(org1,prp2)/(np.linalg.norm(org1)*np.linalg.norm(prp2))
+        c_r_p = np.dot(red1,prp2)/(np.linalg.norm(red1)*np.linalg.norm(prp2))
+
+        org.append(c_org)
+        red.append(c_red)
+        prp.append(c_prp)
+        o_r.append(c_o_r)
+        o_p.append(c_o_p)
+        r_p.append(c_r_p)
+
+avg_org = sum(org)/len(org)
+avg_red = sum(red)/len(red)
+avg_prp = sum(prp)/len(prp)
+avg_or = sum(o_r)/len(o_r)
+avg_op = sum(o_p)/len(o_p)
+avg_rp = sum(r_p)/len(r_p)
+
+
+print("orange to orange avg =")
+print(avg_org)
+print("red to red avg =")
+print(avg_red)
+print("purple to purple avg =")
+print(avg_prp)
+print("orange to red avg =")
+print(avg_or)
+print("orange to purple avg =")
+print(avg_op)
+print("red to purple avg =")
+print(avg_rp)
+
+
+
+
 
 
 
